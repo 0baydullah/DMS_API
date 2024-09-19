e508d564590a5c5f0911924edf6fd8af63434c20
Obaydullah <obaydullahkhaan@gmail.com>
2024-09-20 01:01:10 +0600

    Initial commit

---
+++ src/main/java/com/obaydullah/dms_api/DmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.obaydullah.dms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmsApiApplication.class, args);
+    }
+
+}
